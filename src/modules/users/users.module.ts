2762b2c336576c79a31323360ed988cc94cb8453
Fathy Mohamed <fathymhmed11@gmail.com>
2026-07-26 12:18:15 +0300

    refactor(users): integrate refresh token revocation

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { UsersControllers } from './users.controller';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersRepository } from './users.repository';
+import { RefreshTokensModule } from '../auth/refresh-tokens/refresh-tokens.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersControllers],
+  imports: [TypeOrmModule.forFeature([User]), RefreshTokensModule],
+  controllers: [UsersController],
   providers: [UsersService, UsersRepository],
   exports: [UsersRepository],
 })
